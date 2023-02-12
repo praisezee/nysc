b70da607b01a48cf9156fd9fa080b56112ae1c17
Apus Industries <folorunsopraise580@gmail.com>
2023-02-12 13:14:30 +0100

    modified the height and of the root element and made the form more responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
 
   const year = date.getFullYear()
   return (
-    <footer className='sticky-bottom container-fluid mx-auto text-center bg-success text-white py-0'>
+    <footer className='fixed-bottom container-fluid mx-auto text-center bg-success text-white py-0'>
       <p className='h6'>Designed by <span className='text-uppercase'>Apus Industries</span></p>
       <p className='text-capitalize'>copyright &copy; {year} national youth service corps. all right reserved.| privacy policy</p>
     </footer>
