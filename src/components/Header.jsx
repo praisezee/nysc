7fbd463e67cf75cbdab4073a8aa2e2945767d7f3
Apus Industries <folorunsopraise580@gmail.com>
2023-02-13 21:34:05 +0100

    img rendering fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,9 @@ const Header = () => {
   
   return (
       <Navbar collapseOnSelect expand="lg" bg='light' varient='light' className='sticky-top px-5 shadow'>
-      <Navbar.Brand>
+      <Navbar.Brand className='d-flex'>
         <a href='/' data-rr-ui-event-key='/' className="nav-link d-flex w-50">
-        <img src='https://res.cloudinary.com/dnqjgz03q/image/upload/v1676300530/Nysc/NYSC_LOGO.svg_1_cfvsmr.png' alt="" className='nav-link w-25 expand-image'/>
+        <img src={logo} alt="" className='nav-link w-25 expand-image'/>
         <div className='d-none d-lg-block w-75 my-auto mx-2 slide-in'>
           <p className='text-success text-uppercase h5'>national youth service corp</p>
           <p className='text-danger text-uppercase text-center mx-2'><BsDot className='text-bold h4'/>service and humanity<BsDot className='text-bold h4'/></p>
