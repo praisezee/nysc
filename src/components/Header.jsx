84857fbfb27c2d9b877a54f59f5c95a5946504c9
Apus Industries <folorunsopraise580@gmail.com>
2023-02-14 17:14:20 +0100

    no main update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,23 +8,23 @@ const Header = () => {
 
   
   return (
-      <Navbar collapseOnSelect expand="lg" bg='light' varient='light' className='sticky-top px-5 shadow'>
-      <Navbar.Brand className='d-flex'>
-        <a href='/' data-rr-ui-event-key='/' className="nav-link d-flex w-50">
-        <img src={logo} alt="" className='nav-link img-fluid w-25 expand-image'/>
-        <div className='d-none d-lg-block w-75 my-auto mx-2 slide-in'>
-          <p className='text-success text-uppercase h5'>national youth service corp</p>
-          <p className='text-danger text-uppercase text-center mx-2'><BsDot className='text-bold h4'/>service and humanity<BsDot className='text-bold h4'/></p>
-        </div>
-        
-        </a>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-      <Navbar.Collapse id="responsive-navbar-nav" className="d-lg-flex justify-content-end">
-        <Nav >
-          <ExpandMenu/>
-        </Nav>
-      </Navbar.Collapse>
+  <Navbar collapseOnSelect expand="lg" bg='light' varient='light' className='sticky-top px-5 shadow'>
+    <Navbar.Brand className='d-flex'>
+      <a href='/' data-rr-ui-event-key='/' className="nav-link d-flex w-50">
+      <img src={logo} alt="" className='nav-link img-fluid w-25 expand-image'/>
+      <div className='d-none d-lg-block w-75 my-auto mx-2 slide-in'>
+        <p className='text-success text-uppercase h5'>national youth service corp</p>
+        <p className='text-danger text-uppercase text-center mx-2'><BsDot className='text-bold h4'/>service and humanity<BsDot className='text-bold h4'/></p>
+      </div>
+      
+      </a>
+    </Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+    <Navbar.Collapse id="responsive-navbar-nav" className="d-lg-flex justify-content-end">
+      <Nav >
+        <ExpandMenu/>
+      </Nav>
+    </Navbar.Collapse>
   </Navbar>
   )
 }
