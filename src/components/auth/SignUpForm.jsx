8c0c4dbd3fd25af65931c693cc4c8598060406cc
Apus Industries <folorunsopraise580@gmail.com>
2023-02-15 14:25:02 +0100

    signup componet in action

--- src/components/auth/SignUpForm.jsx
+++ src/components/auth/SignUpForm.jsx
@@ -4,21 +4,6 @@ import Button from './Button'
 import { Row,Col, FormControl, FloatingLabel } from 'react-bootstrap'
 
 const SignUpForm = ({handleSubmit,isSignUp}) => {
-  <form >
-      <div className=" col-3 form-floating mb-3">
-        <input type="email" placeholder='name@example.com' className="form-control" id="floatingInput email" required/>
-        <label htmlFor="email" className="floatingInput"><BsEnvelope className='mx-2 h5 my-auto'/>Email</label>
-      </div>
-      <div className="col-3 form-floating mb-3">
-        <input type="password" placeholder='Enter Your Password' className="form-control" id="floatingInput password" required />
-        <label htmlFor="password" className="floatingInput"><BsLock className='mx-2 h5 my-auto'/>Password</label>
-      </div>
-      <div className="col-3 form-floating mb-3">
-        <input type="password" placeholder='Enter Your Password' className="form-control" id="floatingInput password" required />
-        <label htmlFor="password" className="floatingInput"><BsLock className='mx-2 h5 my-auto'/>Password</label>
-      </div>
-      <Button title='resume' isLogged={isSignUp}/>
-    </form>
   return (
       <form onSubmit={handleSubmit}>
         <Row className='g-2'>
