a9e9e6489fc4340f35d930fec1d5b44e14348c7a
Folorunso Praise <folorunsopraise580@gmail.com>
2023-04-04 04:54:40 +0100

    designed the password update form along side the local government clearance ui

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
